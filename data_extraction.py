8c11e72621f0be38d25d1a41a95d7b5ea7e69ac2
jcapecci09 <jcapecci3@gmail.com>
2026-05-03 07:31:38 -0500

    create code to download UniProt data, however its inefficient may be able to do request in batches

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,15 @@
+"""Script to extract data from DisProt and create two files: 
 
-def parse_consensus(fasta: str) -> tuple[list[str], list[str]]:
+1. Recoded DisProt fasta file
+2. corresponding UniProt fasta file
+
+Author: jcapecci09
+"""
+
+import subprocess
+import os
+
+def parse_consensus(fasta: str, recoded_fasta: str) -> list[str]:
     """Parses consensus IDR data from DisProt. Consensus sequences have the
     following format.
 
@@ -8,22 +18,53 @@ def parse_consensus(fasta: str) -> tuple[list[str], list[str]]:
     -----------------------------------------------------TTTTTTTTTTTTTTTTTTTTTTTTTTT
     TTTTTTTTTT
 
+
     :param fasta: multi-fasta file with consensus seqeunces
-    :return: tuple with two elements: list containing UniProt accessions numbers 
-    & a list containing sequences. 
+    :param recoded_fasta: recoded consensus multi fasta file with D replaced with 1 and [T, -] replaced with 0
+    :return: list of UniProt accession numbers
     """
 
+    # List to contain accessions number
     acc = []
 
-    with open(fasta, 'r') as f:
+    # Open fasta file for reading and new recoded file for 
+    # recoding
+    with open(fasta, 'r') as f, \
+         open(recoded_fasta, 'w') as f1:
+        
+        # For each line in fasta strip the new lines
         for line in f:
             line = line.strip()
 
-            # collect accessions
+            # collect accessions and write headers
             if line.startswith('>'):
+                f1.write(line + '\n')
                 line_split = line.split(' ')
                 acc.append(line_split[1].strip('acc='))
-                
             
-parse_consensus('consensus_IDR.txt')
+            # recode sequences and write to new file
+            else:
+                line_recoded = line.replace('-', '0').replace('D', '1').replace('T', '0')
+                f1.write(line_recoded + '\n')
+    
+    return acc
+
+
+        
+                
+def main():
+
+    # Parse consensus fasta to grab accessions and recode fasta
+    accs = parse_consensus('consensus_IDR.txt', 'recoded_IDR.txt')
+    
+    # create new directory
+    os.makedirs('UniProt_data')
+
+    
+    for acc in accs:
+        subprocess.run(['wget', '-P', 'UniProt_data', f'https://rest.uniprot.org/uniprotkb/{acc}.fasta'])
+
+if __name__ == '__main__':
+    main()
+
 
